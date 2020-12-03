ecd5296339da4a00c93120cc1125988c0aadacbd
Summerz <summerz@pelikinmoney.com>
2020-12-04 09:25:59 +1100

    Initial Commit

---
+++ Pics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pics
+//
+//  Created by 朱莎 on 4/12/20.
+//  Copyright © 2020 Summer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
